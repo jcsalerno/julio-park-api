62e6752e8dbcd15b50647b2c571d6d0d54e6867d
jcsalerno <contato.jcsalerno@gmail.com>
2024-12-18 23:00:14 -0300

    feat: validando o objeto UsuarioSenhaDtot

--- src/main/java/com/julio/park_api/web/dto/UsuarioSenhaDto.java
+++ src/main/java/com/julio/park_api/web/dto/UsuarioSenhaDto.java
@@ -1,4 +1,6 @@
 package com.julio.park_api.web.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -6,7 +8,16 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class UsuarioSenhaDto {
+
+    @NotBlank
+    @Size(min = 6, max= 6)
     private String senhaAtual;
+
+    @NotBlank
+    @Size(min = 6, max= 6)
     private String novaSenha;
+
+    @NotBlank
+    @Size(min = 6, max= 6)
     private String confirmaSenha;
 }
