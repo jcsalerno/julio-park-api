cf9dffabf2e4f98e900a99992a6a8f8c4d0bb709
jcsalerno <contato.jcsalerno@gmail.com>
2024-12-23 19:52:06 -0300

    test: teste para verificar exceções de validações

--- src/test/java/com/julio/park_api/service/UsuarioIT.java
+++ src/test/java/com/julio/park_api/service/UsuarioIT.java
@@ -1,12 +1,18 @@
 package com.julio.park_api.service;
 import com.julio.park_api.web.dto.UsuarioCreateDto;
 import com.julio.park_api.web.dto.mapper.UsuarioResponseDto;
+import com.julio.park_api.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -17,7 +23,7 @@ public class UsuarioIT {
     WebTestClient testClient;
 
     @Test
-    public void createUsuario_ComUserNameEPasswordValidos_RetornarUsuarioCriadoComStatus201(){
+    public void createUsuario_ComUserNameEPasswordValidos_RetornarUsuarioCriadoComStatus201() {
         UsuarioResponseDto responseBody = testClient
                 .post()
                 .uri("/api/v1/usuarios")
@@ -33,4 +39,55 @@ public class UsuarioIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@gmail.com");
         org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
     }
+
+    @ParameterizedTest
+    @MethodSource(value = "invalidUsernames")
+    public void createUsuario_ComUserNameInvalido_RetornarErrorMessageStatus422(String username) {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto(username, "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    private static Stream<Arguments> invalidUsernames() {
+        return Stream.of(
+                Arguments.of(""),
+                Arguments.of("tody@"),
+                Arguments.of("tody@email")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "invalidPasswords")
+    public void createUsuario_ComSenhaInvalida_RetornarErrorMessageStatus422(String password) {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("tody@gmail.com", password))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    private static Stream<Arguments> invalidPasswords() {
+        return Stream.of(
+                Arguments.of("12345"),
+                Arguments.of("1234567"),
+                Arguments.of("abcd")
+        );
+    }
 }
+
