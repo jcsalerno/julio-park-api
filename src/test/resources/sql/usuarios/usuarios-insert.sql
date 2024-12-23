b452193f3174b1e98ecfd68382178001deb04a1c
jcsalerno <contato.jcsalerno@gmail.com>
2024-12-22 21:31:07 -0300

    test: adicionar casos de teste para o serviço de usuários

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (id, username, password, role) values (100, 'ana@email.com.br', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'dga@email.com.br', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (102, 'tga@email.com.br', '123456', 'ROLE_CLIENTE');
