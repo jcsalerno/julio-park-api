bb19265ac12779463018df9cb4cfc34ce593740f
jcsalerno <contato.jcsalerno@gmail.com>
2024-12-24 20:49:15 -0300

    feat: Criando a classe JwtUtils para gerar o token JWT

---
+++ src/main/java/com/julio/park_api/jwt/JwtToken.java
@@ -0,0 +1,12 @@
+package com.julio.park_api.jwt;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtToken {
+
+    private String token;
+}
