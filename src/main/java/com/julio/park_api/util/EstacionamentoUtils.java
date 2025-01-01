7650a0d06818ef51039000eb77691ab161b564c2
jcsalerno <contato.jcsalerno@gmail.com>
2025-01-01 15:19:47 -0300

    feat: método para geração do número do recibo de estacionamento

---
+++ src/main/java/com/julio/park_api/util/EstacionamentoUtils.java
@@ -0,0 +1,16 @@
+package com.julio.park_api.util;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EstacionamentoUtils {
+
+    public static String gerarRecibo() {
+        LocalDateTime date = LocalDateTime.now();
+        String recibo = date.toString().substring(0,19);
+        return recibo.replace("-", "")
+                .replace(":", "")
+                .replace("T", "-");
+    }
+}
